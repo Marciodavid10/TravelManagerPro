38688065bd59848ebbff2bdb30edfff637fec635
Marciodavid10 <davimarcio455@gmail.com>
2026-07-13 13:00:43 +0100

    Corrigir import Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, render_template, request, redirect,redirect, url_for, session, flash, send_file
+﻿from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 from pathlib import Path
 import json
 import smtplib
